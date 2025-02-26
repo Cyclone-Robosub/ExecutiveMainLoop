e0cadba75e4b440f81a77f4a6a9630bc868c43cd
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-02-26 09:23:46 -0800

    Bare setup and init with the main loop.

--- main.cpp
+++ main.cpp
@@ -3,28 +3,39 @@
 #include "SetupConfig/Setup.cpp"
 #include <thread>
 #include <utility>
-
+#include <string>
+#include <chrono>
+using namespace std::literals;
 namespace fs = std::filesystem;
 //start the executive Loop
 class ExecutiveMainLoop{
     public:
-        ExecutiveMainLoop();
-        void ReadInputs();
-        void UpdateState();
-        void ExecuteDecisionLoop();
+        ExecutiveMainLoop() {
+            std::cout << "Done";
+        }
+        void ReadInputs(){std::cout << "Done";}
+        void UpdateState(){std::cout << "Done";}
+        void ExecuteDecisionLoop(){std::cout << "Done";}
         //Sends Commands to Thruster Queue
-        void SendThrusterCommands();
-    private:
+        void SendThrusterCommands(){std::cout << "Done";}
+private:
 };
 int main(){
     //setup Robot during inilization.
     Setup initStateandConfig = Setup();
-    ExecutiveMainLoop mainLoop = ExecutiveMainLoop();
-    std::jthread ReadInputsThread(&ExecutiveMainLoop::ReadInputs, &mainLoop);
-    std::jthread UpdateState(&ExecutiveMainLoop::UpdateState, &mainLoop);
-    std::jthread ExecutiveDecisionLoop(&ExecutiveMainLoop::ExecuteDecisionLoop, &mainLoop);
-    std::jthread SendThrusterCommands(&ExecutiveMainLoop::SendThrusterCommands, &mainLoop);
+    ExecutiveMainLoop mainLoop;
 
+    //can't get std::jthread to work
+    std::thread ReadInputsThread(&ExecutiveMainLoop::ReadInputs, &mainLoop);
+    std::thread UpdateStateThread(&ExecutiveMainLoop::UpdateState, &mainLoop);
+    std::thread ExecutiveDecisionLoopThread(&ExecutiveMainLoop::ExecuteDecisionLoop, &mainLoop);
+    std::thread SendThrusterCommandsThread(&ExecutiveMainLoop::SendThrusterCommands, &mainLoop);
+
+
+    ReadInputsThread.join();
+    UpdateStateThread.join();
+    ExecutiveDecisionLoopThread.join();
+    SendThrusterCommandsThread.join();
 
     return 0;
 }
