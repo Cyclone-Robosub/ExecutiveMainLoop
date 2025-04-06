e392f938bdbe03fd5ad8a629b6748f3df4427fc5
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-04-06 20:00:31 +0000

    just work

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Change directory to src
+# Build the package
+# Source ROS setup files
+source /opt/ros/jazzy/setup.bash
+source install/setup.bash
+
+cd ros2_ws/src
+colcon build
+. install/setup.bash
+cd ../../
+
+source /opt/ros/jazzy/setup.bash
+source install/setup.bash
+colcon build 
+
+
+
+
--- startup.sh
+++ startup.sh
@@ -1,19 +1,11 @@
-#!/bin/bash
-# Change directory to src
-# Build the package
-# Source ROS setup files
 source /opt/ros/jazzy/setup.bash
 source install/setup.bash
 
-cd ros2_ws/src
-colcon build
+cd ros2_ws/src/
 . install/setup.bash
 cd build/inertial_sense_ros2
 ./inertial_sense_ros2_node&
 cd ../../../../
 
-colcon build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
-ros2 run--prefix 'gdbserver localhost:3000' executive_main_loop ExecutiveExecutable 
 
-
-
+ros2 run executive_main_loop ExecutiveExecutable
