f7bc09e276a2047f260baa1952993ab2b7d66306
Dannykcw <106027442+Dannykcw@users.noreply.github.com>
2025-05-02 19:06:52 -0700

    Uncommented test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,17 +1,17 @@
-// /**
-//  * @file test_main.cpp
-//  * @brief Main entry point for Google Test framework
-//  * 
-//  * Initializes and runs all test suites for the Executive Loop system
-//  * 
-//  * @author Danny Kwong
-//  * @date November 7, 2024
-//  */
+/**
+ * @file test_main.cpp
+ * @brief Main entry point for Google Test framework
+ * 
+ * Initializes and runs all test suites for the Executive Loop system
+ * 
+ * @author Danny Kwong
+ * @date November 7, 2024
+ */
 
-//  #include <gtest/gtest.h>
-//  #include "test_nodes.hpp"
+ #include <gtest/gtest.h>
+ #include "test_nodes.hpp"
 
-//  int main(int argc, char** argv) {
-//      testing::InitGoogleTest(&argc, argv);
-//      return RUN_ALL_TESTS();
-//  }
+ int main(int argc, char** argv) {
+     testing::InitGoogleTest(&argc, argv);
+     return RUN_ALL_TESTS();
+ }
