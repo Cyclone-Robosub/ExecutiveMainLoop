60e13a868e264e686b25bfbd4f4e1b209e4012bb
CRS-AGGIES <crs.aggies@gmail.com>
2025-05-16 18:36:08 -0700

    Update submodules script to set branches

--- scripts/submodules.sh
+++ scripts/submodules.sh
@@ -2,3 +2,19 @@ cd ../
 git submodule update --init --recursive --remote
 git submodule sync
 git submodule status
+
+cd lib/Executive_Propulsion
+git checkout master
+
+cd ../Research
+git checkout main
+
+cd ../gtest
+git checkout main
+
+cd ../yaml-cpp
+git checkout master
+
+
+
+git submodule foreach --recursive git pull
