cb03a462c03eb5a6f143dac9a8fb9a46fd960393
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-08-01 09:26:06 -0700

    scripts update

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,26 +8,11 @@ killgroup(){
   kill 0
 }
 
-
-chmod +x submodules.sh
-./submodules.sh
-
 cd ../
 source /opt/ros/jazzy/setup.bash
 
-cd ros2_ws/src 
-colcon build 
-. install/setup.bash
-cd ../../
-
-cd lib/Research/src
-colcon build
-cd ../
-chmod +x startuppy.sh
-cd ../../
-
 source install/setup.bash # in order to source it for the executive build
-colcon build --cmake-args -DMOCK_RPI=OFF
+colcon build --cmake-args -DMOCK_RPI=OFF &
 
 chmod +x startup.sh
 
